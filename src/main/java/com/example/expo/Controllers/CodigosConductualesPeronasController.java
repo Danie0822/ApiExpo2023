74637881e178a6a99e8b769d5d05cc5f9d29bf29
w0lf3 <VRMOTA>
2023-06-30 21:36:09 -0600

    Codigos conductuales personas

---
+++ src/main/java/com/example/expo/Controllers/CodigosConductualesPeronasController.java
@@ -0,0 +1,75 @@
+package com.example.expo.Controllers;
+
+import com.example.expo.Models.CodigosConductualesPersonas;
+import com.example.expo.Models.Encargados;
+import com.example.expo.Models.ServiceResponse;
+import com.example.expo.Services.CodigosConductualesPersonasDB;
+import com.example.expo.Services.EncargadosDB;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+@RequestMapping("CodigosConductualesPersonas")
+public class CodigosConductualesPeronasController {
+    @GetMapping("/list")
+    public List<?> obtenerGrupos() {
+        CompletableFuture<List<?>> futureEspecialidades = new CodigosConductualesPersonasDB().obtenerCodigosConductualesPersonasAsync();
+        List<?> CodigosConductualesPersonas = futureEspecialidades.join();
+        return CodigosConductualesPersonas;
+    }
+
+    @PostMapping("/save")
+    public CompletableFuture<ResponseEntity<ServiceResponse>> save(@RequestBody CodigosConductualesPersonas CodigosConductualesPersonas) {
+        CompletableFuture<Integer> futureResult = CodigosConductualesPersonasDB.insertarCodigosConductualesPersonasAsync(CodigosConductualesPersonas);
+
+        return futureResult.thenApply(result -> {
+            ServiceResponse serviceResponse = new ServiceResponse();
+
+            if (result == 1) {
+                serviceResponse.setMessage("Item saved with success");
+                return ResponseEntity.ok(serviceResponse);
+            } else {
+                serviceResponse.setMessage("Failed to save item");
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(serviceResponse);
+            }
+        });
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CompletableFuture<ResponseEntity<ServiceResponse>> update(@PathVariable int id) {
+        CompletableFuture<Integer> futureResult = CodigosConductualesPersonasDB.eliminarCodigosConductualesPersonasAsync(id);
+
+        return futureResult.thenApply(result -> {
+            ServiceResponse serviceResponse = new ServiceResponse();
+
+            if (result == 1) {
+                serviceResponse.setMessage("Item removed with success");
+                return ResponseEntity.ok(serviceResponse);
+            } else {
+                serviceResponse.setMessage("Failed to remove item");
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(serviceResponse);
+            }
+        });
+    }
+
+    @PutMapping("/update")
+    public CompletableFuture<ResponseEntity<ServiceResponse>> update(@RequestBody CodigosConductualesPersonas CodigosConductualesPersonas) {
+        CompletableFuture<Integer> futureResult = CodigosConductualesPersonasDB.actualizarCodigosConductualesPersonasAsync(CodigosConductualesPersonas);
+
+        return futureResult.thenApply(result -> {
+            ServiceResponse serviceResponse = new ServiceResponse();
+
+            if (result == 1) {
+                serviceResponse.setMessage("Item updated with success");
+                return ResponseEntity.ok(serviceResponse);
+            } else {
+                serviceResponse.setMessage("Failed to update item");
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(serviceResponse);
+            }
+        });
+    }
+}
---
+++ src/main/java/com/example/expo/Services/CodigosConductualesPersonasDB.java
@@ -0,0 +1,191 @@
+package com.example.expo.Services;
+
+import com.example.expo.Models.CodigosConductualesPersonas;
+import com.example.expo.Models.Encargados;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class CodigosConductualesPersonasDB {
+    static Connection _cn;
+    public CodigosConductualesPersonasDB(){
+        _cn = new Conexion().openDB();
+    }
+
+    //SELECT
+    public CompletableFuture<List<?>> obtenerCodigosConductualesPersonasAsync() {
+        return CompletableFuture.supplyAsync(() -> {
+            List<CodigosConductualesPersonas> CodigosConductualesPersonas = new ArrayList<>();
+            Statement statement = null;
+
+            try {
+                statement = _cn.createStatement();
+                ResultSet result = statement.executeQuery("SELECT * FROM tbCodigosConductualesPersonas;");
+
+                while (result.next()) {
+                    CodigosConductualesPersonas codigosConductualesPersonas = new CodigosConductualesPersonas(
+                            result.getInt("idCodigoConductualPersona"),
+                            result.getInt("idEstudiante"),
+                            result.getInt("idCodigoConductual"),
+                            result.getInt("idDocente"),
+                            result.getInt("idPeriodo"),
+                            result.getString("fecha")
+                    );
+
+                    CodigosConductualesPersonas.add(codigosConductualesPersonas);
+                }
+
+                result.close();
+                return CodigosConductualesPersonas;
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return Collections.emptyList();
+            } finally {
+                try {
+                    if (statement != null) {
+                        statement.close();
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).whenComplete((CodigosConductualesPersonas, throwable) -> {
+            try {
+                if (_cn != null && !_cn.isClosed()) {
+                    _cn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    //INSERT
+    public static CompletableFuture<Integer> insertarCodigosConductualesPersonasAsync(CodigosConductualesPersonas CodigosConductualesPersonas) {
+        return CompletableFuture.supplyAsync(() -> {
+            new CodigosConductualesPersonasDB();
+            String sql = "exec AgregarCODCPersonas ?,?,?,?,?;";
+
+            PreparedStatement statement = null;
+            try {
+                statement = _cn.prepareStatement(sql);
+                statement.setInt(1, CodigosConductualesPersonas.getIdEstudiante());
+                statement.setInt(2, CodigosConductualesPersonas.getIdCodigoConductual());
+                statement.setInt(3, CodigosConductualesPersonas.getIdDocente());
+                statement.setInt(4, CodigosConductualesPersonas.getIdPeriodo());
+                statement.setString(5, CodigosConductualesPersonas.getFecha());
+                statement.executeUpdate();
+
+                return 1;
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return 0;
+            } finally {
+                try {
+                    if (statement != null && !statement.isClosed()) {
+                        statement.close();
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).whenComplete((result, throwable) -> {
+            try {
+                if (_cn != null && !_cn.isClosed()) {
+                    _cn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+
+    //DELETE
+    public static CompletableFuture<Integer> eliminarCodigosConductualesPersonasAsync(int id) {
+        return CompletableFuture.supplyAsync(() -> {
+            new CodigosConductualesPersonasDB();
+            String sql = "exec DeleteCODCPersonas ?;";
+            PreparedStatement statement = null;
+            try {
+                statement = _cn.prepareStatement(sql);
+                statement.setInt(1, id);
+                statement.executeUpdate();
+                return 1;
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return 0;
+            } finally {
+                try {
+                    if (statement != null) {
+                        statement.close();
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).whenComplete((result, throwable) -> {
+            try {
+                if (_cn != null && !_cn.isClosed()) {
+                    _cn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    //UPDATE
+    public static CompletableFuture<Integer> actualizarCodigosConductualesPersonasAsync(CodigosConductualesPersonas CodigosConductualesPersonas) {
+        return CompletableFuture.supplyAsync(() -> {
+            new CodigosConductualesPersonasDB();
+
+            String sql = "exec  ?,?,?,?,?,?;";
+            PreparedStatement statement = null;
+            try  {
+                statement = _cn.prepareStatement(sql);
+                statement.setInt(1, CodigosConductualesPersonas.getIdCodigoConductualPersona());
+                statement.setInt(2, CodigosConductualesPersonas.getIdEstudiante());
+                statement.setInt(3, CodigosConductualesPersonas.getIdCodigoConductual());
+                statement.setInt(4, CodigosConductualesPersonas.getIdDocente());
+                statement.setInt(5, CodigosConductualesPersonas.getIdPeriodo());
+                statement.setString(6,CodigosConductualesPersonas.getFecha());
+
+                statement.executeUpdate();
+                return 1;
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return 0;
+            }
+            finally {
+                try {
+                    if (statement != null) {
+                        statement.close();
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).whenComplete((result, throwable) -> {
+            try {
+                if (_cn != null && !_cn.isClosed()) {
+                    _cn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+}
