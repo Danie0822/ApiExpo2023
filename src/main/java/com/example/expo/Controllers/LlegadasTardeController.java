d629665a6504d4de330619f47962c57d4d702468
yaciraaa <20190142@ricaldone.edu.sv>
2023-07-03 23:01:59 -0600

    LlegadasTarde

---
+++ src/main/java/com/example/expo/Controllers/LlegadasTardeController.java
@@ -0,0 +1,77 @@
+package com.example.expo.Controllers;
+import com.example.expo.Models.Grados;
+import com.example.expo.Models.GruposTecnicos;
+import com.example.expo.Models.ServiceResponse;
+import com.example.expo.Models.TiposLlegadasTarde;
+import com.example.expo.Services.GradosDB;
+import com.example.expo.Services.GruposTecnicosDB;
+import com.example.expo.Services.LlegadasTardeDB;
+import com.example.expo.Services.TiposLlegadasTardeDB;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+@RequestMapping("LlegadasTarde")
+public class LlegadasTardeController {
+
+    @GetMapping("/list")
+    public List<?> obtenerGrupos() {
+        CompletableFuture<List<?>> futureEspecialidades = new LlegadasTardeDB().obtenerLlegadasTardeAsync();
+        List<?> LlegadasTarde = futureEspecialidades.join();
+        return LlegadasTarde;
+    }
+
+    @PostMapping("/save")
+    public CompletableFuture<ResponseEntity<ServiceResponse>> save(@RequestBody LlegadasTarde LlegadasTarde) {
+        CompletableFuture<Integer> futureResult = LlegadasTardeDB.insertarLlegadasTardeAsync(LlegadasTarde);
+
+        return futureResult.thenApply(result -> {
+            ServiceResponse serviceResponse = new ServiceResponse();
+
+            if (result == 1) {
+                serviceResponse.setMessage("Item saved with success");
+                return ResponseEntity.ok(serviceResponse);
+            } else {
+                serviceResponse.setMessage("Failed to save item");
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(serviceResponse);
+            }
+        });
+    }
+    @DeleteMapping("/delete/{id}")
+    public CompletableFuture<ResponseEntity<ServiceResponse>> update(@PathVariable int id) {
+        CompletableFuture<Integer> futureResult = LlegadasTardeDB.eliminarLlegadasTardeAsync(id);
+        return futureResult.thenApply(result -> {
+            ServiceResponse serviceResponse = new ServiceResponse();
+
+            if (result == 1) {
+                serviceResponse.setMessage("Item removed with success");
+                return ResponseEntity.ok(serviceResponse);
+            } else {
+                serviceResponse.setMessage("Failed to remove item");
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(serviceResponse);
+            }
+        });
+    }
+
+    @PutMapping("/update")
+    public CompletableFuture<ResponseEntity<ServiceResponse>> update(@RequestBody LlegadasTarde LlegadasTarde) {
+        CompletableFuture<Integer> futureResult = LlegadasTardeDB.ActualizarLlegadasTardeAsync(LlegadasTarde);
+
+        return futureResult.thenApply(result -> {
+            ServiceResponse serviceResponse = new ServiceResponse();
+
+            if (result == 1) {
+                serviceResponse.setMessage("Item updated with success");
+                return ResponseEntity.ok(serviceResponse);
+            } else {
+                serviceResponse.setMessage("Failed to update item");
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(serviceResponse);
+            }
+        });
+    }
+}
+}
---
+++ src/main/java/com/example/expo/Models/LlegadasTarde.java
@@ -0,0 +1,23 @@
+package com.example.expo.Models;
+
+public class LlegadasTarde {
+    int idLlegadaTarde;
+    int idTipoLlegadaTarde;
+    int idPersona;
+    int idPeriodo;
+    int idDocente;
+    String TiposLlegadasTarde;
+
+
+    public LlegadasTarde(int idLlegadaTarde, int idTipoLlegadaTarde, int idPersona, int idPeriodo, int idDocente, String TiposLlegadasTarde) {
+        this.idLlegadaTarde = idLlegadaTarde;
+        this.idTipoLlegadaTarde= idTipoLlegadaTarde;
+        this.idPersona = idPersona;
+        this.idPeriodo = idPeriodo;
+        this.idDocente = idDocente;
+        this.TiposLlegadasTarde = TiposLlegadasTarde;
+    }
+
+
+}
+
